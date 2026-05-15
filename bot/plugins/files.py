fda192bbfb52bdc17098713697c864f19746bdfb
JeevabharathiRK <JeevabharathiRK@gmail.com>
2026-05-15 16:17:51 +0000

    test 1

--- bot/plugins/files.py
+++ bot/plugins/files.py
@@ -53,6 +53,7 @@ async def handle_user_file(_, msg: Message):
             # Generate stream link for videos
             if (replied_message.document and 'video' in replied_message.document.mime_type) or replied_message.video:
                 stream_link = f'{Server.BASE_URL}/stream/{file_id}?code={secret_code}'
+                vlc_link = f'{Server.BASE_URL}/vlc/{file_id}?code={secret_code}'
                 await msg.reply(
                     text=MediaLinksText % {'dl_link': dl_link, 'stream_link': stream_link},
                     quote=True,
@@ -60,7 +61,8 @@ async def handle_user_file(_, msg: Message):
                         [
                             [
                                 InlineKeyboardButton('Download', url=dl_link),
-                                InlineKeyboardButton('Stream', url=stream_link)
+                                InlineKeyboardButton('Stream', url=stream_link),
+                                InlineKeyboardButton('VLC', url=vlc_link)
                             ]
                         ]
                     )
@@ -136,12 +138,14 @@ async def handle_storage_channel_file(client, msg: Message):
 
     if is_video:
         stream_link = f"{Server.BASE_URL}/stream/{file_id}?code={secret_code}"
+        vlc_link = f"{Server.BASE_URL}/vlc/{file_id}?code={secret_code}"
         text = MediaLinksText % {"dl_link": dl_link, "stream_link": stream_link}
         keyboard = InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("Download", url=dl_link),
                     InlineKeyboardButton("Stream", url=stream_link),
+                    InlineKeyboardButton("VLC", url=vlc_link),
                 ]
             ]
         )
